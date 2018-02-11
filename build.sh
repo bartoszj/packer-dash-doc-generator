75107d06a1f403a63c9c8386f174d68d45fdb2ab
Bartosz Janda <bartosz.janda@gmail.com>
2018-02-11 20:24:21 +0100

    Code refactor

--- build.sh
+++ build.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
 
-set -x
+set -ex
 
-tag=$1
-if [ -z $tag ]; then
-    echo '"tag" must be specified'
+TAG=$1
+if [ -z $TAG ]; then
+    echo '"TAG" must be specified'
     exit 1
 fi
 
 CWD=$(pwd)
-BUILD_PATH="$CWD/build/$tag"
-PACKER_PATH="$CWD/packer"
-WEBSITE_PATH=$PACKER_PATH/website
+BUILD_PATH="${CWD}/build/$TAG"
+PACKER_PATH="${CWD}/packer"
+WEBSITE_PATH="${PACKER_PATH}/website"
 
-rm -rf $BUILD_PATH
-mkdir -p $BUILD_PATH
+rm -rf "${BUILD_PATH}"
+mkdir -p "${BUILD_PATH}"
 
-git clone https://github.com/hashicorp/packer.git || true
-cd $PACKER_PATH
+git clone "https://github.com/hashicorp/packer.git" || true
+cd "${PACKER_PATH}"
 git fetch --all --prune
-git checkout v${tag}
+git checkout -- .
+git checkout "v${TAG}"
 
-cd $WEBSITE_PATH
+cd "${WEBSITE_PATH}"
 bundle install
 
 rm Rakefile || true
-# cp $CWD/Rakefile .
-ln -s $CWD/Rakefile || true
+# cp "${CWD}/Rakefile" .
+ln -s "${CWD}/Rakefile" || true
 
 rake
 
-mv Packer.tgz $BUILD_PATH
+mv Packer.tgz "${BUILD_PATH}"
