3b8bdf6b05435a07c923d5573293b9792584e7be
Bartosz Janda <bartosz.janda@gmail.com>
2018-03-04 19:26:21 +0100

    Readme.md updates

--- Readme.md
+++ Readme.md
@@ -7,7 +7,7 @@ This projects essence has been taken from [https://github.com/rolandjohann/terra
 
 ```bash
 rbenv install 2.5.0
-gem install bundler rake sqlite3
+gem install -N bundler rake sqlite3
 ```
 
 ### Build
--- build.sh
+++ build.sh
@@ -2,26 +2,31 @@
 
 set -ex
 
+# Read parameters
 TAG=$1
 if [ -z $TAG ]; then
     echo '"TAG" must be specified'
     exit 1
 fi
 
+# Paths
 CWD=$(pwd)
 BUILD_PATH="${CWD}/build/$TAG"
 PACKER_PATH="${CWD}/packer"
 WEBSITE_PATH="${PACKER_PATH}/website"
 
+# Clean build
 rm -rf "${BUILD_PATH}"
 mkdir -p "${BUILD_PATH}"
 
+# Checkout and clean
 git clone "https://github.com/hashicorp/packer.git" || true
 cd "${PACKER_PATH}"
 git fetch --all --prune
 git checkout -- .
 git checkout "v${TAG}"
 
+# Install gems
 cd "${WEBSITE_PATH}"
 bundle install
 
@@ -29,6 +34,7 @@ rm Rakefile || true
 # cp "${CWD}/Rakefile" .
 ln -s "${CWD}/Rakefile" || true
 
+# Build
 rake
 
 mv Packer.tgz "${BUILD_PATH}"
